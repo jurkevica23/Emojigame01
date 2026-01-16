b2fac7aa6e3f3d868081b8566c2f229f93210b58
jurkevica23 <ljurkevica1@anip.lv>
2026-01-16 09:09:37 +0200

    16.01.2026

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,3 +1,23 @@
 //no URL iegUst vArdu
 let adrese = window.location.hash.substring(1);
-adrese = decodeURI(adrese.split(',')[0])
+let vards = decodeURI(adrese.split(',')[0]   '').trim();
+
+//mainīgie spēles darbībai
+let laiks = 0; //sekundes
+let kliski = 0;
+
+//taimera mainīgie (taimeris strādās ar pirmo kliski)
+let timerId = null;
+let timerStarted = false;
+
+
+
+
+
+
+const laukumiSaturs = ['😙','🥸','👽','😺','😙','🥶','😇','🥶','🥸','😺','👽','😇'];
+let atvertilaukumi = [];
+let pedejieDivi = [];
+
+//sajauc emoji nejaušā secībā
+let laukumiSajaukti = [...laukumiSaturs].sort(() => Math.random() - 0.5);
