1d64e0636c9060d0c3ed45fc5ae69a3c301acc66
jurkevica23 <ljurkevica1@anip.lv>
2026-02-06 10:05:29 +0200

    06.02.26

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, rember_template, request, jsonify
-from pathlib Path 
+from flask import Flask, render_template, request, jsonify
+from pathlib import Path 
 import json
 
 base_dir = Path(__file__).resolve().parent
-app = Flask(__name__, template_folder=str(base_dir / 'templates'), static_folder=str(base_dir / 'static'))                                                 
+app = Flask(__name__, template_folder=str(base_dir / 'templates'), static_folder=str(base_dir / 'static'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/spele')
+def spele():
+    return render_template('game.html')
+
+@app.route('/tops')
+def tops():
+    return render_template('top.html')
+
+@app.route('/par')
+def par():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
